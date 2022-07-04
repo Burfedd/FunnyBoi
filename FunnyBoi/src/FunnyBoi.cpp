f5f6900b5542e54b11320fc09629e01b9552cf77
Burfedd <leonquad2@gmail.com>
2022-07-04 14:24:24 +0300

    Initial Bot creation - Ping Pong example

--- FunnyBoi/src/FunnyBoi.cpp
+++ FunnyBoi/src/FunnyBoi.cpp
@@ -1,7 +1,26 @@
 ﻿#include "FunnyBoi.h"
 
+const std::string BOT_TOKEN = "bot token";
+const std::string GUILD_ID = "guild id";
+
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+	dpp::cluster bot(BOT_TOKEN);
+	bot.on_log(dpp::utility::cout_logger());
+
+	bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+		if (event.command.get_command_name() == "ping") {
+			event.reply("Pong!");
+		}
+	});
+
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+		if (dpp::run_once<struct register_bot_commands>()) {
+			bot.global_command_create(
+				dpp::slashcommand("ping", "Ping Pong!", bot.me.id)
+			);
+		}
+	});
+
+	bot.start(false);
 }

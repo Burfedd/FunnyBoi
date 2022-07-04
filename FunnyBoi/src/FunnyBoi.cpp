1a7ef19d11bf80424761a8d48fc17bcbb8590bad
Burfedd <leonquad2@gmail.com>
2022-07-04 18:10:17 +0300

    Random reply functionality

--- FunnyBoi/src/FunnyBoi.cpp
+++ FunnyBoi/src/FunnyBoi.cpp
@@ -5,12 +5,26 @@ const std::string GUILD_ID = "guild id";
 
 int main()
 {
+	// Random
+	std::random_device r_device;
+	std::mt19937 rng(r_device());
+	std::uniform_int_distribution<std::mt19937::result_type> dist(0, 100);
+
+	// Bot
 	dpp::cluster bot(BOT_TOKEN);
+
 	bot.on_log(dpp::utility::cout_logger());
 
 	bot.on_slashcommand([](const dpp::slashcommand_t& event) {
 		if (event.command.get_command_name() == "ping") {
 			event.reply("/tts test");
+		}
+	});
+
+	bot.on_message_create([&dist, &rng](const dpp::message_create_t& event) {
+		int generated = dist(rng);
+		if (generated >= 50) {
+			event.reply("Random reply");
 		}
 	});
 
